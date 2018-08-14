34ad4661252d3d678b1e23ee650ffdda8fdd3a59
amirezabp <arfahimibp@gmail.com>
2018-08-14 16:32:26 +0430

    add category fragment

---
+++ app/src/main/java/com/yaratech/yaratube/ui/category/CategoryFragment.java
@@ -0,0 +1,35 @@
+package com.yaratech.yaratube.ui.category;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yaratech.yaratube.R;
+
+
+public class CategoryFragment extends Fragment {
+
+    public CategoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static CategoryFragment newInstance() {
+        CategoryFragment fragment = new CategoryFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false);
+    }
+}
